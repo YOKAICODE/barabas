2b59c9ec2761437ed4199745a52010d3fff1621e
yokaicode <yokaicode>
2024-09-11 21:01:55 +0300

    Сделал смену города

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,8 @@
 import "./modules/isWebp.js";
 // import "./modules/scrollOffset.js";
 import "./modules/tab.js";
-import "./modules/swiper.js"
-import "./modules/sidebar.js"
-import "./modules/frame.js"
-import "./modules/changeCity.js"
+import "./modules/swiper.js";
+import "./modules/sidebar.js";
+import "./modules/frame.js";
+import "./modules/changeCity.js";
+import "./modules/dropdown.js";
--- src/js/modules/changeCity.js
+++ src/js/modules/changeCity.js
@@ -2,11 +2,27 @@ const cityBtnEl = document.querySelector("#changeCity");
 const closeBtn = document.querySelector("#closeCity");
 const cityFrameEl = document.querySelector("#changeCityFrame");
 const bodyEl = document.querySelector("body");
+const selectCityEl = document.querySelector("#selectCity");
+
+const setCity = (city) => {
+  // Устанавливаем куку и перезагружаем страницу
+  document.cookie = "city=" + city + ";max-age='3600';path=/";
+  location.reload();
+};
+
+const checkOfside = (e) => {
+  const contentElement = e.target.closest(".content");
+  if (!contentElement) {
+    closeCityModal();
+  }
+};
 
 const closeCityModal = () => {
   cityFrameEl.classList.add("hidden");
   cityFrameEl.classList.remove("flex");
   bodyEl.classList.remove("overflow-hidden", "h-screen");
+  closeBtn.removeEventListener("click", closeCityModal);  
+  cityFrameEl.removeEventListener("click", checkOfside);
   cityBtnEl.addEventListener("click", openCityModal);
 };
 
@@ -15,6 +31,11 @@ const openCityModal = () => {
   cityFrameEl.classList.add("flex");
   bodyEl.classList.add("overflow-hidden", "h-screen");
   closeBtn.addEventListener("click", closeCityModal);
+  cityFrameEl.addEventListener("click", checkOfside);
 };
 
 cityBtnEl.addEventListener("click", openCityModal);
+
+selectCityEl.addEventListener("change", (evt) => {
+  setCity(evt.target.value);
+});
